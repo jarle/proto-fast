dce99c2fe73439cc005dd500797056de6c409959
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2025-10-19 07:57:42 +0200

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import devtools from 'vite-plugin-devtools-json';
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    devtools()
+  ],
 });
